158ed59c08f2436a603c9feba815086be1a04de9
Austin Jackson <themacphage@gmail.com>
2020-05-17 19:20:50 -0500

    im stupid

--- install.sh
+++ install.sh
@@ -56,6 +56,16 @@ editor $WORKDIR/parrot.env
 echo "Creating plugins folder"
 sudo mkdir -p /mnt/minecraft/plugins
 
+# Downloading plugins
+PLUGINS=$WORKDIR/plugins/*
+for f in $PLUGINS
+do
+  echo "Downloading plugin $f ..."
+  URL=$(cat $f)
+  wget "$URL" -P /mnt/minecraft/plugins
+done
+echo "\n"
+
 # Start the containers
 echo "Starting containers"
 sudo docker-compose --file "$WORKDIR/docker-compose.yml" up -d
@@ -64,32 +74,21 @@ sudo docker-compose --file "$WORKDIR/docker-compose.yml" up -d
 sudo docker container update --restart unless-stopped aerogel_minecraft_1
 sudo docker container update --restart unless-stopped aerogel_parrot_1
 
-# Wait for server to create plugins folder
-echo "Waiting for server to generate plugins folder"
-while [ ! -d /mnt/minecraft/plugins ]
-do
-	echo `date`" plugins not ready yet";
-	sleep 2;
-done
+# # Wait for server to create plugins folder
+# echo "Waiting for server to generate plugins folder"
+# while [ ! -d /mnt/minecraft/plugins ]
+# do
+# 	echo `date`" plugins not ready yet";
+# 	sleep 2;
+# done
 
-# Wait for server to process plugins
-echo "Waiting for server to process plugins folder"
-while [ ! -d /mnt/minecraft/plugins/bStats ]
-do
-	echo `date`" bStats not ready yet";
-	sleep 2;
-done
-
-# Downloading plugins
-PLUGINS=$WORKDIR/plugins/*
-for f in $PLUGINS
-do
-  echo "Downloading plugin $f ..."
-  URL=$(cat $f)
-  wget "$f" -P /mnt/minecraft/plugins
-  cat $f
-done
-echo "\n"
+# # Wait for server to process plugins
+# echo "Waiting for server to process plugins folder"
+# while [ ! -d /mnt/minecraft/plugins/bStats ]
+# do
+# 	echo `date`" bStats not ready yet";
+# 	sleep 2;
+# done
 
 # Restarting server to use plugins
 sudo docker container restart aerogel_minecraft_1
