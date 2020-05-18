6df65410b3fc294a852a33a48ff5c685f8ff55d0
Austin Jackson <themacphage@gmail.com>
2020-05-17 19:23:02 -0500

    i think thats it

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ for f in $PLUGINS
 do
   echo "Downloading plugin $f ..."
   URL=$(cat $f)
-  wget "$URL" -P /mnt/minecraft/plugins
+  wget "$URL" -q --show-progress -P /mnt/minecraft/plugins
 done
 echo "\n"
 
@@ -73,25 +73,6 @@ sudo docker-compose --file "$WORKDIR/docker-compose.yml" up -d
 # Configure the restart policy because docker-compose 3.0+ is retarded and won't let us do it ourselves
 sudo docker container update --restart unless-stopped aerogel_minecraft_1
 sudo docker container update --restart unless-stopped aerogel_parrot_1
-
-# # Wait for server to create plugins folder
-# echo "Waiting for server to generate plugins folder"
-# while [ ! -d /mnt/minecraft/plugins ]
-# do
-# 	echo `date`" plugins not ready yet";
-# 	sleep 2;
-# done
-
-# # Wait for server to process plugins
-# echo "Waiting for server to process plugins folder"
-# while [ ! -d /mnt/minecraft/plugins/bStats ]
-# do
-# 	echo `date`" bStats not ready yet";
-# 	sleep 2;
-# done
-
-# Restarting server to use plugins
-sudo docker container restart aerogel_minecraft_1
 
 # Setting permissions for job script
 echo "Setting permissions for autoshutdown.sh"
